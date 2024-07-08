67a71e572d15c93c37a8023169993df168646385
Vaibhav Diwan <divaibhav@gmail.com>
2024-07-08 12:36:39 +0530

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,33 @@
+// c++ program structure
+// single line comment
+/*
+multiline
+commment
+*/
+//cpp program always starts with header file 
+#include <iostream>
+//using the namespace standard 
+using namespace std;
+// define main method, as main method is starting piont of 
+// c++ program
+int main(){
+// print helloworld on console / output
+    //cout is used to send the output on the console
+    cout << "hello world" << endl;
+    cout << "Vaibhav" << "\n";
+    cout << "c++";
+    
+}
+//code
+//save
+//compile
+// if error, correct your code
+//if no errors
+//execute
+// syntax to compile
+// g++ filename.cpp
+// after compilation and output file will be generated;
+// i.e. an exe file on windows, the default name for the 
+// file is a.exe
+// syntax to run .exe or output file
+// ./outputfilename
